ec34885aa6af25ee870c520892f90bd2a76896b8
kajal <tiwarikajal1306@gmail.com>
2020-09-11 00:08:53 +0530

    REFACTORED Welcome msg

---
+++ controller/Ipl_Controller.h
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+class Ipl_Controller 
+{
+    public :
+    void show_Welcome_Msg();
+};
+
+void Ipl_Controller :: show_Welcome_Msg()
+{
+    cout << "Welcome to IPL Analyser" << endl;
+}
